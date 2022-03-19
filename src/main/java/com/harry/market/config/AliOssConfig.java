1488852397bee664aec61ca9529aa20442616050
Sky_Dog <362664609@qq.com>
2022-03-20 00:27:41 +0800

    oss

---
+++ src/main/java/com/harry/market/config/AliOssConfig.java
@@ -0,0 +1,29 @@
+package com.harry.market.config;
+
+import com.aliyun.oss.OSSClient;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class AliOssConfig {
+    // 地域节点
+    @Value("${aliyun.oss.endpoint}")
+    private String endpoint;
+    @Value("${aliyun.accessKeyId}")
+    private String accessKeyId;
+    @Value("${aliyun.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
+    @Value("${aliyun.oss.urlPrefix}")
+    private String urlPrefix;
+
+    @Bean
+    public OSSClient ossClient() {
+        return new OSSClient(endpoint, accessKeyId, accessKeySecret);
+    }
+}
---
+++ src/main/java/com/harry/market/service/UploadPicService.java
@@ -0,0 +1,35 @@
+package com.harry.market.service;
+
+import com.aliyun.oss.OSSClient;
+import com.harry.market.config.AliOssConfig;
+import com.harry.market.utils.OssUtills;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@Service
+public class UploadPicService {
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Autowired
+    private AliOssConfig aliyunConfig;
+
+    /**
+     *
+     * @param multipartFile
+     * @param fileName
+     * @param folder
+     * @return 图片url
+     * @throws FileNotFoundException
+     */
+    public String uploadPic(MultipartFile multipartFile,String fileName, String folder) throws FileNotFoundException {
+        InputStream  inputstream  = new  FileInputStream(fileName);
+        return OssUtills.saveImg(multipartFile,ossClient,folder);
+    }
+}
