863539b0234ee873c042f5dab3c648622b348919
Sky_Dog <362664609@qq.com>
2022-03-21 22:24:46 +0800

    fix springsecurity

--- src/main/java/com/harry/market/config/WebSecurityConfig.java
+++ src/main/java/com/harry/market/config/WebSecurityConfig.java
@@ -1,13 +1,12 @@
 package com.harry.market.config;
 
-import com.harry.market.handler.CustomizeAuthenticationEntryPoint;
-import com.harry.market.handler.CustomizeAuthenticationFailureHandler;
-import com.harry.market.handler.CustomizeAuthenticationSuccessHandler;
-import com.harry.market.handler.CustomizeLogoutSuccessHandler;
+import com.harry.market.handler.*;
 import com.harry.market.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,22 +15,46 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    CustomizeAuthenticationEntryPoint authenticationEntryPoint;
-
+    //登录成功处理逻辑
     @Autowired
     CustomizeAuthenticationSuccessHandler authenticationSuccessHandler;
 
+    //登录失败处理逻辑
     @Autowired
     CustomizeAuthenticationFailureHandler authenticationFailureHandler;
 
+    //权限拒绝处理逻辑
+    @Autowired
+    CustomizeAccessDeniedHandler accessDeniedHandler;
+
+    //匿名用户访问无权限资源时的异常
+    @Autowired
+    CustomizeAuthenticationEntryPoint authenticationEntryPoint;
+
+    //会话失效(账号被挤下线)处理逻辑
+    @Autowired
+    CustomizeSessionInformationExpiredStrategy sessionInformationExpiredStrategy;
+
+    //登出成功处理逻辑
     @Autowired
     CustomizeLogoutSuccessHandler logoutSuccessHandler;
+
+    //访问决策管理器
+    @Autowired
+    CustomizeAccessDecisionManager accessDecisionManager;
+
+    //实现权限拦截
+    @Autowired
+    CustomizeFilterInvocationSecurityMetadataSource securityMetadataSource;
+
+    @Autowired
+    private CustomizeAbstractSecurityInterceptor securityInterceptor;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -48,6 +71,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginProcessingUrl("/login");
         //http相关的配置，包括登入登出、异常处理、会话管理等
 //        super.configure(http);
         //放行swagger相关接口
@@ -63,22 +88,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin();
         //放行注册接口
-        http
-                .authorizeRequests()
-                .antMatchers("/user/register").permitAll()
-                .antMatchers("/user/login").permitAll()
-                .anyRequest().authenticated();
-        http.cors().and().exceptionHandling().
-                //登入
-                    and().formLogin().
-                    permitAll().//允许所有用户
-                    successHandler(authenticationSuccessHandler).//登录成功处理逻辑
-                    failureHandler(authenticationFailureHandler).//登录失败处理逻辑
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().
+                //antMatchers("/getUser").hasAuthority("query_user").
+                //antMatchers("/**").fullyAuthenticated().
+                        withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+                    @Override
+                    public <O extends FilterSecurityInterceptor> O postProcess(O o) {
+//                        AccessDecisionManager accessDecisionManager;
+                        o.setAccessDecisionManager(accessDecisionManager);//决策管理器
+                        o.setSecurityMetadataSource(securityMetadataSource);//安全元数据源
+                        return o;
+                    }
+                }).
                 //登出
-                    and().logout().
-                    permitAll().//允许所有用户
-                    logoutSuccessHandler(logoutSuccessHandler).//登出成功处理逻辑
-                    deleteCookies("JSESSIONID");//登出之后删除cookie
+                        and().logout().
+                permitAll().//允许所有用户
+                logoutSuccessHandler(logoutSuccessHandler).//登出成功处理逻辑
+                deleteCookies("JSESSIONID").//登出之后删除cookie
+                //登入
+                        and().formLogin().
+                permitAll().//允许所有用户
+                successHandler(authenticationSuccessHandler).//登录成功处理逻辑
+                failureHandler(authenticationFailureHandler).//登录失败处理逻辑
+                //异常处理(权限拒绝、登录失效等)
+                        and().exceptionHandling().
+                accessDeniedHandler(accessDeniedHandler).//权限拒绝处理逻辑
+                authenticationEntryPoint(authenticationEntryPoint).//匿名用户访问无权限资源时的异常处理
+                //会话管理
+                        and().sessionManagement().
+                maximumSessions(1).//同一账号同时登录最大用户数
+                expiredSessionStrategy(sessionInformationExpiredStrategy);//会话失效(账号被挤下线)处理逻辑
+        http.addFilterBefore(securityInterceptor, FilterSecurityInterceptor.class);
     }
 
     @Override
---
+++ src/main/java/com/harry/market/handler/CustomizeAccessDeniedHandler.java
@@ -0,0 +1,32 @@
+package com.harry.market.handler;
+
+
+import com.alibaba.fastjson.JSON;
+import com.harry.market.common.Constants;
+import com.harry.market.common.Result;
+import com.harry.market.common.ResultCode;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 天狗
+ * @description 权限拒绝处理逻辑
+ */
+
+@Component
+public class CustomizeAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        Result result = Result.error(Constants.CODE_401,ResultCode.NO_PERMISSION);
+        httpServletResponse.setContentType("text/json;charset=utf-8");
+        httpServletResponse.getWriter().write(JSON.toJSONString(result));
+    }
+}
